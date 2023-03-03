cad1b9771569877cf8e463b8b471b3c4f9e640d4
Marcello <marcelloaltermundi@gmail.com>
2023-03-03 17:35:50 -0300

    feat: add requisito 02

---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        display_name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: DataTypes.STRING,
+    },
+    {
+        timestamps: false,
+        tableName: 'users'
+    });
+
+    User.associate = (models) => {
+        User.hasMany(models.Blog_post, { foreignKey: 'user_id', as: 'blog_posts' })
+    };
+
+    return User;
+}
