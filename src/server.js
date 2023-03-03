6c75f2306ed9ae46c2b8a04178cffa35dc6e3bd4
Marcello <marcelloaltermundi@gmail.com>
2023-03-03 14:48:36 -0300

    iniciando projeto

--- src/server.js
+++ src/server.js
@@ -4,4 +4,4 @@ const app = require('./app');
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
 
-app.listen(port, () => console.log('ouvindo porta', port));
+app.listen(port, () => console.log('ouvindo na porta', port));
