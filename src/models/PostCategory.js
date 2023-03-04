68d6739466efaf43190df70f0a59f6e5b77cd393
Marcello <marcelloaltermundi@gmail.com>
2023-03-04 19:59:36 -0300

    feat: add requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define('PostCategory',
+    {
+        postId: DataTypes.INTEGER,
+        categoryId: DataTypes.INTEGER
+    },
+    {
+        timestamps: false,
+        underscored: true,
+        tableName: 'posts_categories'
+    });
+
+    PostCategory.associate = (models) => {
+        models.BlogPost.belongsToMany(models.Category, {
+            as: 'categories',
+            through: 'posts_categories',
+            foreignKey: 'post_id',
+            otherKey: 'category_id'
+        });
+        models.Category.belongsToMany(models.BlogPost, {
+            as: 'blog_posts',
+            through: 'posts_categories',
+            foreignKey: 'category_id',
+            otherKey: 'post_id'
+        });
+
+    }
+    return PostCategory;
+}
